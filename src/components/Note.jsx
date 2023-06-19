9a3b808d90e491f9347b3653d03061ddc3c7eee6
Deji Obasan <dejiobasan@yahoo.com>
2023-06-19 13:02:56 +0100

    created my first React App

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+    return (
+    <div className="note">
+        <h1>Introduction to React</h1>
+        <p>React is a good library for dveeloping UI elements.</p>
+    </div>
+    );
+}
+
+
+export default Note;
