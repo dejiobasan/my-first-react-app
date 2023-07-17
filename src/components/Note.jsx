51943620b354d028867c0c590f4d3c59482c255c
Deji Obasan <dejiobasan@yahoo.com>
2023-07-17 17:00:54 +0100

    .

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Note() {
+function Note(props) {
     return (
     <div className="note">
-        <h1>Introduction to React</h1>
-        <p>React is a good library for developing UI elements.</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
     );
 }
 
 
-export default Note();
+export default Note;
