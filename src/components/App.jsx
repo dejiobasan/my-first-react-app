b9f3b62123428573f2e454dc672759682e5a6046
Deji Obasan <dejiobasan@yahoo.com>
2023-08-02 14:55:22 +0100

    Created my first React app.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import  notes  from "../notes.js"
+// import  notes  from "../notes.js";
+import CreateArea from "./CreateArea";
 
 
-function createNotes(notesTerm) {
-    return <Note 
-        key = {notesTerm.id}
-        title = {notesTerm.tilte}
-        content = {notesTerm.content}
-    />;
-}
+function App() {
 
-function App(){
+    const [notes, setNotes] = useState([ ]);
+
+    function addNote(newNote) {
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+    };
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    };
+    
+
     return (
-    <div>
-        <Header />
-        {notes.map(createNotes)}
-        <Footer />
-    </div>
+        <div>
+            <Header />
+            <CreateArea onAdd={addNote} />
+            {
+                notes.map((noteItem, index) => {
+                    return (<Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote} />);
+                })
+            }
+            <Footer />
+        </div>
     );
 }
 
